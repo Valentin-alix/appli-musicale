655012c29e3f87a17ee503a1a30a12456c8d7595
Samuel Challe <samuel.challe@gmail.com>
2022-02-27 13:04:07 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,14 @@ import 'package:application_musicale/screens/util/material_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 
+import 'models/album.dart';
 import 'models/artist.dart';
 
 Future<void> main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(ArtistAdapter());
-  await Hive.openBox('FavouritesArtistsBox');
-  await Hive.openBox('FavouritesAlbumsBox');
-
+  Hive.registerAdapter(AlbumAdapter());
+  await _openBox();
   runApp(const MyApp());
 }
 
@@ -30,4 +30,13 @@ class MyApp extends StatelessWidget {
       onGenerateRoute: AppRoutes.getRoutes,
     );
   }
+}
+
+List<Box> boxList = [];
+Future<List<Box>> _openBox() async {
+  var favouritesArtistsBox = await Hive.openBox("FavouritesArtistsBox");
+  var favouritesAlbumsBox = await Hive.openBox("FavouritesAlbumsBox");
+  boxList.add(favouritesArtistsBox);
+  boxList.add(favouritesAlbumsBox);
+  return boxList;
 }
--- lib/screens/favourites/favourites.dart
+++ lib/screens/favourites/favourites.dart
@@ -1,17 +1,33 @@
+import 'package:application_musicale/models/album.dart';
+import 'package:application_musicale/models/artist.dart';
 import 'package:application_musicale/screens/item/albums_list_item.dart';
 import 'package:application_musicale/screens/item/artists_list_item.dart';
-import 'package:application_musicale/screens/model/albums_data.dart';
-import 'package:application_musicale/screens/model/artists_data.dart';
 import 'package:application_musicale/screens/util/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 
-class Favourites extends StatelessWidget {
+class Favourites extends StatefulWidget {
   const Favourites({Key? key}) : super(key: key);
 
   @override
+  FavouritesState createState() => FavouritesState();
+}
+
+class FavouritesState extends State<Favourites> {
+  late Future<List> futureFavouritesArtists;
+  late Future<List> futureFavouritesAlbums;
+
+  @override
+  initState() {
+    super.initState();
+    insertData();
+    futureFavouritesArtists = getFavouritesArtistsList();
+    futureFavouritesAlbums = getFavouritesAlbumsList();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    List<ArtistsData> artistsResults = _generateArtistsResults();
-    List<AlbumsData> albumsResults = _generateAlbumsResults();
     const appBarTitle = 'Favoris';
     const appBarSubtitle = 'Mes artistes & albums';
     const bodyTitle1 = 'Artistes';
@@ -65,18 +81,39 @@ class Favourites extends StatelessWidget {
                   ),
                 ),
                 const Divider(),
-                ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: albumsResults.length,
-                  itemBuilder: (BuildContext context, int position) {
-                    return ArtistsListItem(
-                      picture: artistsResults[position].picture,
-                      title: artistsResults[position].artists,
-                      artistId: "111261",
-                    );
-                  },
-                ),
+                FutureBuilder<List>(
+                    future: futureFavouritesArtists,
+                    builder: (context, AsyncSnapshot<List> snapshot) {
+                      if (snapshot.hasData) {
+                        return ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: snapshot.data == null
+                                ? 0
+                                : snapshot.data?.length,
+                            itemBuilder: (BuildContext context, int position) {
+                              return ArtistsListItem(
+                                picture:
+                                    snapshot.data![position].strArtistThumb,
+                                title: snapshot.data![position].strArtist,
+                                artistId: snapshot.data![position].artistId,
+                              );
+                            });
+                      } else if (snapshot.hasError) {
+                        return const Text('Une erreur est survenue !');
+                      } else {
+                        return const Center(
+                          child: SizedBox(
+                            child: CircularProgressIndicator(
+                              valueColor: AlwaysStoppedAnimation<Color>(
+                                  UIColors.suvaGrey),
+                            ),
+                            height: 50.0,
+                            width: 50.0,
+                          ),
+                        );
+                      }
+                    }),
                 const SizedBox(height: 20),
                 const Text(
                   bodyTitle2,
@@ -87,19 +124,39 @@ class Favourites extends StatelessWidget {
                   ),
                 ),
                 const Divider(),
-                ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: albumsResults.length,
-                  itemBuilder: (BuildContext context, int position) {
-                    return AlbumsListItem(
-                      picture: albumsResults[position].picture,
-                      title: albumsResults[position].album,
-                      subtitle: albumsResults[position].artists,
-                      albumId: "2109902",
-                    );
-                  },
-                ),
+                FutureBuilder<List>(
+                    future: futureFavouritesAlbums,
+                    builder: (context, AsyncSnapshot<List> snapshot) {
+                      if (snapshot.hasData) {
+                        return ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: snapshot.data == null
+                                ? 0
+                                : snapshot.data?.length,
+                            itemBuilder: (BuildContext context, int position) {
+                              return AlbumsListItem(
+                                picture: snapshot.data![position].strAlbumThumb,
+                                title: snapshot.data![position].strAlbum,
+                                subtitle: snapshot.data![position].strArtist,
+                                albumId: snapshot.data![position].albumId,
+                              );
+                            });
+                      } else if (snapshot.hasError) {
+                        return const Text('Une erreur est survenue !');
+                      } else {
+                        return const Center(
+                          child: SizedBox(
+                            child: CircularProgressIndicator(
+                              valueColor: AlwaysStoppedAnimation<Color>(
+                                  UIColors.suvaGrey),
+                            ),
+                            height: 50.0,
+                            width: 50.0,
+                          ),
+                        );
+                      }
+                    }),
               ],
             ),
           ),
@@ -108,77 +165,30 @@ class Favourites extends StatelessWidget {
     );
   }
 
-  List<ArtistsData> _generateArtistsResults() {
-    return [
-      ArtistsData(
-        artists: 'Creedence Clearwater Revival',
-        picture:
-            'https://upload.wikimedia.org/wikipedia/commons/e/ee/Creedence_Clearwater_Revival_1968.jpg',
-      ),
-      ArtistsData(
-        artists: 'Creedence Clearwater Revival',
-        picture: '',
-      ),
-      ArtistsData(
-        artists: 'Creedence Clearwater Revival',
-        picture: '',
-      ),
-      ArtistsData(
-        artists: 'Creedence Clearwater Revival',
-        picture: '',
-      ),
-      ArtistsData(
-        artists: 'Creedence Clearwater Revival',
-        picture: '',
-      ),
-      ArtistsData(
-        artists: 'Creedence Clearwater Revival',
-        picture: '',
-      ),
-      ArtistsData(
-        artists: 'Creedence Clearwater Revival',
-        picture: '',
-      ),
-      ArtistsData(
-        artists: 'Creedence Clearwater Revival2',
-        picture: '',
-      ),
-    ];
+  Future<void> insertData() async {
+    final favouritesArtistsBox = Hive.box('FavouritesArtistsBox');
+    final favouritesAlbumsBox = Hive.box('FavouritesAlbumsBox');
+
+    Artist artist1 = Artist('111279', "ABBA",
+        "https://www.theaudiodb.com/images/media/artist/thumb/qyuqvy1375623610.jpg");
+    await favouritesArtistsBox.add(artist1);
+
+    Artist artist2 = Artist('111258', "Metallica",
+        "https://www.theaudiodb.com/images/media/artist/thumb/qyuqvy1375623610.jpg");
+    await favouritesArtistsBox.add(artist2);
+
+    Album album1 = Album('2110231', "Lulu", "Metallica",
+        "https://www.theaudiodb.com/images/media/album/thumb/lulu-4f872f3f80ff9.jpg");
+    await favouritesAlbumsBox.add(album1);
   }
 
-  List<AlbumsData> _generateAlbumsResults() {
-    return [
-      AlbumsData(
-        rank: 1,
-        album: 'Willy And The Poor Boys',
-        artists: 'Creedence Clearwater Revival',
-        picture:
-            'https://upload.wikimedia.org/wikipedia/commons/e/ee/Creedence_Clearwater_Revival_1968.jpg',
-      ),
-      AlbumsData(
-        rank: 1,
-        album: 'Green River',
-        artists: 'Creedence Clearwater Revival',
-        picture: '',
-      ),
-      AlbumsData(
-        rank: 1,
-        album: 'Green River',
-        artists: 'Creedence Clearwater Revival',
-        picture: '',
-      ),
-      AlbumsData(
-        rank: 1,
-        album: 'Green River',
-        artists: 'Creedence Clearwater Revival',
-        picture: '',
-      ),
-      AlbumsData(
-        rank: 1,
-        album: 'Green River',
-        artists: 'Creedence Clearwater Revival',
-        picture: '',
-      ),
-    ];
+  Future<List> getFavouritesArtistsList() async {
+    final favouritesBox = Hive.box('FavouritesArtistsBox');
+    return favouritesBox.values.toList();
+  }
+
+  Future<List> getFavouritesAlbumsList() async {
+    final favouritesAlbumsBox = Hive.box('FavouritesAlbumsBox');
+    return favouritesAlbumsBox.values.toList();
   }
 }
