91db79143f3dfbec855aa2b072f5d2886d81c378
Samuel Challe <samuel.challe@gmail.com>
2022-02-27 10:45:22 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,16 @@ import 'package:application_musicale/routes.dart';
 import 'package:application_musicale/screens/util/colors.dart';
 import 'package:application_musicale/screens/util/material_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+import 'models/artist.dart';
+
+Future<void> main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(ArtistAdapter());
+  await Hive.openBox('FavouritesArtistsBox');
+  await Hive.openBox('FavouritesAlbumsBox');
+
   runApp(const MyApp());
 }
 
